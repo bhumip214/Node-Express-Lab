d271cfc4857f6bcfd8cb13376647491cd78b23d4
bhumip214 <bhumip214@gmail.com>
2019-02-12 13:23:50 -0800

    Added Post request

--- posts-router.js
+++ posts-router.js
@@ -39,4 +39,24 @@ router.get("/:id", (req, res) => {
     });
 });
 
+// POST request
+router.post("/", (req, res) => {
+  const { title, contents } = req.body;
+  if (!title || !contents) {
+    res.status(400).json({
+      errorMessage: "Please provide title and contents for the post."
+    });
+  } else {
+    Posts.insert({ title, contents })
+      .then(post => {
+        res.status(201).json(post);
+      })
+      .catch(err => {
+        res.status(500).json({
+          error: "There was an error while saving the post to the database"
+        });
+      });
+  }
+});
+
 module.exports = router;
