6168c63ac833614764554f62ef3b42ee7dd2103a
bhumip214 <bhumip214@gmail.com>
2019-02-12 13:10:05 -0800

    Get request done

--- posts-router.js
+++ posts-router.js
@@ -6,4 +6,17 @@ const router = express.Router();
 
 router.use(express.json());
 
+// GET request
+router.get("/", (req, res) => {
+  Posts.find()
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: "The posts information could not be retrieved."
+      });
+    });
+});
+
 module.exports = router;
