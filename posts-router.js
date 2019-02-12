566ac5e805b4b69830756cedaa64fc8c5d228f42
bhumip214 <bhumip214@gmail.com>
2019-02-12 13:46:11 -0800

    Put request done and completed all MVP

--- posts-router.js
+++ posts-router.js
@@ -78,4 +78,34 @@ router.delete("/:id", (req, res) => {
     });
 });
 
+// UPDATE request
+router.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const { title, contents } = req.body;
+
+  if (!title || !contents) {
+    res.status(400).json({
+      errorMessage: "Please provide title and contents for the post."
+    });
+  } else {
+    Posts.update(id, { title, contents })
+      .then(post => {
+        if (post) {
+          res.status(200).json(post);
+        } else {
+          res
+            .status(404)
+            .json({
+              message: "The post with the specified ID does not exist."
+            });
+        }
+      })
+      .catch(err => {
+        res.status(500).json({
+          error: "The post information could not be modified."
+        });
+      });
+  }
+});
+
 module.exports = router;
