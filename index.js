9826408ddcd58909c9f66fb334b010cef6ec3841
bhumip214 <bhumip214@gmail.com>
2019-02-14 13:50:40 -0800

    made changes to deploy on heroku

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+require("dotenv").config();
+
 const server = require("./server");
 
-server.listen(5000, () => {
-  console.log("\n*** Running on Port 5000 ***\n");
+const port = process.env.PORT || 5000;
+
+server.listen(port, () => {
+  console.log(`\n*** Running on Port ${port} ***\n`);
 });
